b3d551494f52ae487864570b44e8ea4e7f2478e8
Bernd Müller <github@muellerbernd.de>
2025-02-14 07:46:21 +0100

    [change] update encoder keymap for kimiko keyboard

--- keyboards/muellerbernd/kimiko/keymaps/bernd/keymap.c
+++ keyboards/muellerbernd/kimiko/keymaps/bernd/keymap.c
@@ -147,11 +147,11 @@ bool encoder_update_user(uint8_t index, bool clockwise)
         // Volume control
         if (clockwise)
         {
-            tap_code(KC_VOLD);
+            tap_code(KC_VOLU);
         }
         else
         {
-            tap_code(KC_VOLU);
+            tap_code(KC_VOLD);
         }
     }
     else if (index == 1)
