a65a49b1d7e2b0bfddc3adda4c411ea2b967d4b9
Bernd Müller <github@muellerbernd.de>
2025-03-18 23:33:10 +0100

    [add] custom bento version

---
+++ keyboards/muellerbernd/bento/keymaps/default/keymap.c
@@ -0,0 +1,36 @@
+#include QMK_KEYBOARD_H
+
+enum encoder_names {
+  _ENCODER
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+        |                   |      | Knob 1: Vol Dn/Up  |
+        | Hold: Layer 2     | Up   | Press: Mute        |
+        | Left              | Down | Right              |
+     */
+    [0] = LAYOUT(
+        MO(1), KC_UP, KC_MUTE,
+        KC_LEFT, KC_DOWN, KC_RGHT
+    ),
+    /*
+        | Held: Layer 2  | Home | QK_BOOT      |
+        | Media Previous | End  | Media Next |
+     */
+    [1] = LAYOUT(
+        _______, KC_HOME, QK_BOOT,
+        KC_MPRV, KC_END , KC_MNXT
+    ),
+};
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == _ENCODER) {
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+    }
+    return true;
+}
---
+++ keyboards/muellerbernd/bento/rules.mk
@@ -0,0 +1,1 @@
+DEFAULT_FOLDER = muellerbernd/bento/rev1
